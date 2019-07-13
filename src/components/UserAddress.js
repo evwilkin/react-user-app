8f400f19692468c098c40e9b4d446ca05eea3680
evwilkin <evan.wilkinson@gmail.com>
2019-07-13 17:31:27 -0400

    added city to UserAddress by zipcode lookup - only 3 zips are valid US cities

--- src/components/UserAddress.js
+++ src/components/UserAddress.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import zipcodes from 'zipcodes';
 
-const UserAddress = ({ address }) => (
-  <React.Fragment>
-    <p>{address.street}</p>
-    <p>{address.suite}</p>
-    <p>{address.city} {address.zipcode}</p>
-  </React.Fragment>
-);
+class UserAddress extends Component {
+  state = {
+    state: false
+  }
+
+  componentDidMount = async () => {
+    try {
+      const zip = this.props.address.zipcode.substr(0,5);
+      const zipLookup = await zipcodes.lookup(zip);
+      const state = zipLookup.state;
+      this.setState({ state });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <p>{this.props.address.street}</p>
+        <p>{this.props.address.suite}</p>
+        <p>{this.props.address.city}{this.state.state ? `, ${this.state.state}` : null} {this.props.address.zipcode}</p>
+      </React.Fragment>
+    );
+  }
+}
 
 export default UserAddress;
