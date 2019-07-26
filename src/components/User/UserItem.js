015c210a1bd5b41e8981cb672fbb017e7fdae5b2
evwilkin <evan.wilkinson@gmail.com>
2019-07-25 22:38:53 -0400

    converted UserItem from class component to stateless functional component

--- src/components/User/UserItem.js
+++ src/components/User/UserItem.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ToggleWrapper from './ToggleWrapper';
 import UserItemHeader from './UserItemHeader/UserItemHeader';
 import PropTypes from 'prop-types';
 
-export default class UserItem extends Component {
-  render () {
-    const { user, objKey } = this.props;
-    let n = 0;
-    const getKey = () => `${Date.now()}-${n++}`;
-    const buildNestedTable  = (obj, objKey) => {
-      const isNestedObject = typeof obj[objKey] === 'object';
+const UserItem = props => {
+  const { user, objKey } = props;
+  let n = 0;
+  const getKey = () => `${Date.now()}-${n++}`;
+  const buildNestedTable  = (obj, objKey) => {
+    const isNestedObject = typeof obj[objKey] === 'object';
 
-      if (isNestedObject) {
-        const nestedObject = obj[objKey];
-        return (
-          <ToggleWrapper
+    if (isNestedObject) {
+      const nestedObject = obj[objKey];
+      return (
+        <ToggleWrapper
+          objKey={objKey}
+          obj={obj}
+          nestedObject={nestedObject}
+          buildNestedTable={buildNestedTable}
+          key={getKey()}
+        />
+      )
+    } else {
+      return (
+        <div key={getKey()} className="list-group-item">
+          <UserItemHeader
             objKey={objKey}
             obj={obj}
-            nestedObject={nestedObject}
-            buildNestedTable={buildNestedTable}
-            key={getKey()}
+            isNestedObject={false}
           />
-        )
-      } else {
-        return (
-          <div key={getKey()} className="list-group-item">
-            <UserItemHeader
-              objKey={objKey}
-              obj={obj}
-              isNestedObject={false}
-            />
-          </div>
-        )
-      }
+        </div>
+      )
     }
-
-    const userItems = buildNestedTable(user, objKey);
-
-    return userItems;
   }
+  const userItems = buildNestedTable(user, objKey);
+
+  return userItems;
 }
+
+export default UserItem;
 
 UserItem.propTypes = {
   objKey: PropTypes.string,
